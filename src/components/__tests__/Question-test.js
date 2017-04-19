0b8ecb4f3b3af04f7bc6d60e37c2dd1ba11f45df
Robin Schaaf <robin@zooniverse.org>
2017-04-19 12:11:04 -0500

    Add Task Help

--- src/components/__tests__/Question-test.js
+++ src/components/__tests__/Question-test.js
@@ -4,12 +4,24 @@ import renderer from 'react-test-renderer'
 jest.mock('WebView', () => 'WebView')
 import { Question } from '../Question'
 
-it('renders correctly', () => {
+it('renders correctly (without task help)', () => {
   const tree = renderer.create(
     <Question
       question={'Is this this?'}
       questionContainerHeight={25}
       setQuestionContainerHeight={jest.fn}
+      />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders correctly with task help', () => {
+  const tree = renderer.create(
+    <Question
+      question={'Is this this?'}
+      questionContainerHeight={25}
+      setQuestionContainerHeight={jest.fn}
+      taskHelp={'Halp!'}
       />
   ).toJSON()
   expect(tree).toMatchSnapshot()
