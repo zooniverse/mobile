876342f3378cddcf4695b0e92a4aa75d1424b869
Robin Schaaf <robin@zooniverse.org>
2017-04-25 11:03:53 -0500

    Fix broken test

--- src/constants/mobile_projects.js
+++ src/constants/mobile_projects.js
@@ -1,6 +1,10 @@
-export const MOBILE_PROJECTS = ['21', '55', '60', '2299', '347', '1338', '36', '1457', '17', '2455', '2492', '2977', '3219', '2501']
+export const MOBILE_PROJECTS = ['60', '2299', '347', '1338', '36', '1457', '17', '2455', '2492', '3219', '3321', '2501', '14', '1558']
 
 export const SWIPE_WORKFLOWS = [
+  { projectID: '3321', workflowID: '2725' },
+  { projectID: '3321', workflowID: '3437' },
   { projectID: '2501', workflowID: '3577' },
   { projectID: '3219', workflowID: '2627' },
+  { projectID: '14', workflowID: '433' },
+  { projectID: '1558', workflowID: '3335' },
 ]
