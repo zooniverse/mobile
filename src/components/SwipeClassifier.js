ceea13068a667fdad9422ffb58b29435b39338b0
Will Granger <twgranger@gmail.com>
2017-06-05 12:26:14 -0500

    Don't Initiate Tutorial if Non-existent  (#127)
    * Only Initiate Tutorial if One Exists in Props
    
    * Set needsTutorial to False if no Tutorial

--- src/components/SwipeClassifier.js
+++ src/components/SwipeClassifier.js
@@ -183,7 +183,6 @@ export class SwipeClassifier extends React.Component {
             handlePress={() => this.setState({ showFullSize: false })} />
         </View>
 
-      //needsTutorial is for the first time a guest or user visits this project
       return (
         this.props.needsTutorial ? tutorial : classificationPanel
       )
