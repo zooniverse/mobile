d7cfa7ead6ce733ef1317a799e31ee13d47b368e
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-02-22 13:40:42 -0600

    Use correct redux devtool for react native

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'remote-redux-devtools';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { AppState, NetInfo } from 'react-native'
 import { Provider } from 'react-redux'
 import reducer from '../reducers/index'
