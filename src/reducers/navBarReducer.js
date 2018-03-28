94bf05f9963bcbc914567b57953a7446b2b7e9ee
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-03-28 10:37:50 -0500

    Add nav bar actions to control background color

--- src/reducers/navBarReducer.js
+++ src/reducers/navBarReducer.js
@@ -2,7 +2,8 @@ import * as ActionConstants from '../constants/actions';
 import * as R from 'ramda'
 
 const InitialNavBarState = {
-    titles: {}
+    titles: {},
+    backgroundColors: {}
 };
 
 export default function navBar(state=InitialNavBarState, action) {
@@ -10,6 +11,15 @@ export default function navBar(state=InitialNavBarState, action) {
         case ActionConstants.SET_NAVBAR_TITLE: {
             const titles = R.set(R.lensProp(action.pageKey), action.title, state.titles);
             return { ...state, titles }
+        }
+        case ActionConstants.SET_NAVBAR_COLOR: {
+            const backgroundColors = R.set(R.lensProp(action.pageKey), action.color, state.backgroundColors);
+            return { ...state, backgroundColors}
+        }
+        case ActionConstants.SET_NAVBAR_COLOR_TO_DEFAULT: {
+            const backgroundColors = state.backgroundColors
+            delete backgroundColors[action.pageKey]
+            return { ...state, backgroundColors}
         }
         default:
             return state;
