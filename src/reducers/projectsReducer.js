d9215a1b6eb012cd88ad2372e846cab8df84c386
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-03-28 10:50:38 -0500

    Update Projects Actions for beta projects

--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -5,31 +5,49 @@ const InitialProjectState = {
     isLoading: false,
     isSuccess: false,
     isFailure: false,
-    projectList: []
+    projectList: [],
+    betaProjectList: [],
+    collaboratorIds: [],
+    ownerIds: []
 };
 
 export default function projects(state=InitialProjectState, action) {
     switch (action.type) {
-        case ActionConstants.ADD_PROJECTS_REQUEST: {
+        case ActionConstants.PROJECTS_REQUEST: {
             return {
                 ...state,
                 isLoading: true
             }
         }
-        case ActionConstants.ADD_PROJECTS_SUCCESS: {
+        case ActionConstants.PROJECTS_SUCCESS: {
             return {
                 ...state,
                 isLoading: false,
                 isSuccess: true,
                 isFailure: false,
-                projectList: action.projects
             };
         }
-        case ActionConstants.ADD_PROJECTS_FAILURE: {
+        case ActionConstants.ADD_PROJECTS: {
+            const { areBeta, projects } = action
+            return areBeta ? { ...state, betaProjectList: projects } : { ...state, projectList: projects}
+        }
+        case ActionConstants.PROJECTS_FAILURE: {
             return {
                 ...state,
                 isLoading: false,
                 isFailure: true
+            }
+        }
+        case ActionConstants.ADD_OWNER_PROJECT_ID: {
+            return {
+                ...state,
+                ownerIds: R.append(action.projectId, state.ownerIds)
+            }
+        }
+        case ActionConstants.ADD_COLLABORATOR_PROJECT_ID: {
+            return {
+                ...state,
+                collaboratorIds: R.append(action.projectId, state.collaboratorIds)
             }
         }
         default:
