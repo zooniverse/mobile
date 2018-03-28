c2d1205139c1da1df2d9075657b188a714e0ef71
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-03-28 10:57:24 -0500

    Update Tests

--- src/components/__tests__/ProjectDiscipline-test.js
+++ src/components/__tests__/ProjectDiscipline-test.js
@@ -5,10 +5,14 @@ import { ProjectDisciplines } from '../ProjectDisciplines'
 
 jest.useFakeTimers();
 
+const projectActions = {
+  fetchProjectsWithTags: () => {}
+}
+
 it('renders correctly', () => {
   const user ={ display_name: 'Fake User' }
   const tree = renderer.create(
-    <ProjectDisciplines user={user} />
+    <ProjectDisciplines user={user} projectList={[]} projectActions={projectActions} />
   ).toJSON()
   expect(tree).toMatchSnapshot()
 })
