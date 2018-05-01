a9a92106787c5f1d71d52947f13235c68583ca80
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-05-01 10:24:16 -0500

    Add Swiper Library and modal into the classifier
    Also add in some styling updates

--- src/components/classifier/SwipeClassifier.js
+++ src/components/classifier/SwipeClassifier.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {
-  StyleSheet,
+  Animated,
+  Dimensions,
+  TouchableOpacity,
   View
 } from 'react-native'
 import PropTypes from 'prop-types';
@@ -10,33 +12,41 @@ import { bindActionCreators } from 'redux'
 import ClassificationPanel from './ClassificationPanel'
 import Question from './Question'
 import Tutorial from './Tutorial'
-import Swipeable from './Swipeable'
-import SwipeSubject from './SwipeSubject'
 import SwipeTabs from './SwipeTabs'
-import OverlaySpinner from './OverlaySpinner'
-import NavBar from './NavBar'
-import FullScreenImage from './FullScreenImage'
+import OverlaySpinner from '../OverlaySpinner'
+import NavBar from '../NavBar'
+import FullScreenImage from '../FullScreenImage'
 import UnlinkedTask from './UnlinkedTask'
-import { append, contains, isEmpty, reverse, uniq } from 'ramda'
-import * as classifierActions from '../actions/classifier'
-import * as navBarActions from '../actions/navBar'
-import Theme from '../theme'
+import Swiper from 'react-native-deck-swiper'
+import R from 'ramda'
+import * as classifierActions from '../../actions/classifier'
+import * as navBarActions from '../../actions/navBar'
+import Theme from '../../theme'
+import SwipeCard from './SwipeCard'
+import TaskHelpModal from './TaskHelpModal'
+import FontedText from '../common/FontedText'
+import { getTaskFromWorkflow, getAnswersFromWorkflow } from '../../utils/workflow-utils'
 
+export const subjectDisplayHeight = Dimensions.get('window').height - 380
+export const subjectDisplayWidth = Dimensions.get('window').width - 80
 
-const mapStateToProps = (state, ownProps) => ({
-  isSuccess: state.classifier.isSuccess,
-  isFailure: state.classifier.isFailure,
-  isFetching: state.classifier.isFetching,
-  annotations: state.classifier.annotations[ownProps.workflow.id] || {},
-  guide: state.classifier.guide[ownProps.workflow.id] || {},
-  tutorial: state.classifier.tutorial[ownProps.workflow.id] || {},
-  needsTutorial: state.classifier.needsTutorial[ownProps.workflow.id] || false,
-  subject: state.classifier.subject[ownProps.workflow.id] || {},
-  nextSubject: state.classifier.nextSubject[ownProps.workflow.id] || {},
-  subjectDisplayWidth: state.main.device.subjectDisplayWidth,
-  subjectDisplayHeight: state.main.device.subjectDisplayHeight,
-  seenThisSession: state.classifier.seenThisSession[ownProps.workflow.id] || [],
-})
+const mapStateToProps = (state, ownProps) => {
+  return {
+    task: getTaskFromWorkflow(ownProps.workflow),
+    answers: R.reverse(getAnswersFromWorkflow(ownProps.workflow)),
+    isSuccess: state.classifier.isSuccess,
+    isFailure: state.classifier.isFailure,
+    isFetching: state.classifier.isFetching,
+    annotations: state.classifier.annotations[ownProps.workflow.id] || {},
+    guide: state.classifier.guide[ownProps.workflow.id] || {},
+    tutorial: state.classifier.tutorial[ownProps.workflow.id] || {},
+    needsTutorial: state.classifier.needsTutorial[ownProps.workflow.id] || false,
+    subjectDisplayWidth: state.main.device.subjectDisplayWidth,
+    subjectDisplayHeight: state.main.device.subjectDisplayHeight,
+    subjectLists: state.classifier.subjectLists[ownProps.workflow.id] || [],
+    subjectsSeenThisSession: state.classifier.seenThisSession[ownProps.workflow.id] || []
+ }
+}
 
 const mapDispatchToProps = (dispatch) => ({
   classifierActions: bindActionCreators(classifierActions, dispatch),
@@ -51,8 +61,15 @@ export class SwipeClassifier extends React.Component {
     this.state = {
       isQuestionVisible: true,
       showFullSize: false,
+      isModalVisible: false,
+      swiperIndex: 0,
+      fullScreenImageSource: '',
+      panX: null
     }
     this.props.classifierActions.startNewClassification(this.props.workflow, this.props.project)
+    this.onAnswered = this.onAnswered.bind(this)
+    this.onSwiped = this.onSwiped.bind(this)
+    this.onTapCard = this.onTapCard.bind(this)
   }
 
   setQuestionVisibility(isVisible) {
@@ -67,16 +84,45 @@ export class SwipeClassifier extends React.Component {
     }
   }
 
-  onAnswered = (answer) => {
+  onAnswered = (answer, subject) => {
     const { id, first_task } = this.props.workflow
     this.props.classifierActions.addAnnotationToTask(id, first_task, answer, false)
-    this.props.classifierActions.saveThenStartNewClassification(this.props.workflow)
+    this.props.classifierActions.saveClassification(this.props.workflow, subject)
+  }
+
+  onSwiped = (subjectIndex) => {
+    this.setState({ swiperIndex: this.state.swiperIndex + 1 })
+    if (subjectIndex > this.props.subjectLists.length - 8) {
+      this.props.classifierActions.addSubjectsForWorklow(this.props.workflow.id)
+    }
+  }
+
+  onTapCard = (cardIndex) => {
+    this.setState({
+      showFullSize: true,
+      fullScreenImageSource: this.props.subjectLists[cardIndex].display.src
+    })
+  }
+
+  renderCard = (subject) => {
+      const seenThisSession = R.indexOf(subject.id, this.props.subjectsSeenThisSession) >= 0
+      const shouldAnimateOverlay = this.props.subjectLists[this.state.swiperIndex].id === subject.id
+
+      return this.state.panX ? (
+          <SwipeCard 
+            subject={subject}
+            seenThisSession={seenThisSession}
+            panX={this.state.panX}
+            shouldAnimateOverlay={shouldAnimateOverlay}
+            answers={this.props.answers}
+          />
+      ) : <View />
   }
 
   onUnlinkedTaskAnswered = (task, value) => {
     const taskAnnotations = this.props.annotations[task] || []
     const { id } = this.props.workflow
-    if (contains(value, taskAnnotations)) {
+    if (R.contains(value, taskAnnotations)) {
       this.props.classifierActions.removeAnnotationFromTask(id, task, value)
     } else {
       this.props.classifierActions.addAnnotationToTask(id, task, value, true)
@@ -99,106 +145,130 @@ export class SwipeClassifier extends React.Component {
   }
 
   render() {
-    const renderClassifierOrTutorial = () => {
-      const key = this.props.workflow.first_task //always just one task
-      const task = this.props.workflow.tasks[key]
-      const answers = reverse(task.answers)  //Yes is listed first in project, but we need No listed first (on left)
-      const allowPanAndZoom = this.props.workflow.configuration.pan_and_zoom
-
-      const unlinkedTask = task.unlinkedTask
-        ? <UnlinkedTask
-            unlinkedTaskKey={ task.unlinkedTask }
-            unlinkedTask={ this.props.workflow.tasks[task.unlinkedTask] }
-            annotation={ this.props.annotations[task.unlinkedTask] }
-            onAnswered={ this.onUnlinkedTaskAnswered }/>
-        : null
-      const subjectSizes = {
-        resizedHeight: this.props.subjectDisplayHeight,
-        resizedWidth: this.props.subjectDisplayWidth
-      }
-      const backSubject =
-        <SwipeSubject
-          inFront={false}
-          subject={this.props.nextSubject}
-          subjectSizes={subjectSizes}
-          seenThisSession={this.props.seenThisSession}
-          setImageSizes={() => {}}
-        />
-
-      const classification =
-        <View>
-          <Question
-            question={task.question}
-            workflowID={this.props.workflow.id}
-            taskHelp={task.help}
-          />
-          <View style={[styles.subjectContainer, { width: this.props.subjectDisplayWidth, height: this.props.subjectDisplayHeight }]}>
-            { isEmpty(this.props.nextSubject) ? null : backSubject }
-          </View>
-        </View>
-
-      const swipeableSubject =
-        <Swipeable
-          key={this.props.subject.id}
-          workflow={this.props.workflow}
-          onAnswered={this.onAnswered}
-          answers={answers}
-          showFullSize={() => this.setState({showFullSize: true})}
-        />
-
-      const swipeTabs =
-        <SwipeTabs
-          guide={this.props.guide}
-          onAnswered={this.onAnswered}
-          answers={answers}
-        />
-
-      const tutorial =
-        <Tutorial
-          projectName={this.props.project.display_name}
-          isInitialTutorial={this.props.needsTutorial}
-          tutorial={this.props.tutorial}
-          finishTutorial={() => this.finishTutorial()}
-        />
-
-      //swipeable subject needs to be outside the Classification Panel because of Android
-      //the swipeable area is cut off to the panel otherwise
-      const classificationPanel =
-        <View style={{...StyleSheet.absoluteFillObject}}>
-          <ClassificationPanel
-            isFetching={ this.props.isFetching }
-            hasTutorial = { !isEmpty(this.props.tutorial) }
-            isQuestionVisible = {this.state.isQuestionVisible }
-            setQuestionVisibility = { this.setQuestionVisibility }>
-            { this.state.isQuestionVisible ? classification : tutorial }
-            { this.state.isQuestionVisible ? unlinkedTask :null }
-          </ClassificationPanel>
-          { this.state.isQuestionVisible ? swipeableSubject : null }
-          { this.state.isQuestionVisible ? swipeTabs : null }
-          <FullScreenImage
-            source={{uri: this.props.subject.display.src}}
-            isVisible={this.state.showFullSize}
-            allowPanAndZoom={allowPanAndZoom}
-            handlePress={() => this.setState({ showFullSize: false })} />
-        </View>
-
-      return (
-        this.props.needsTutorial ? tutorial : classificationPanel
-      )
+    if (this.props.isFetching || !this.props.isSuccess) {
+      return <OverlaySpinner overrideVisibility={this.props.isFetching} />
     }
+
+    const tutorial =
+      <Tutorial
+        projectName={this.props.project.display_name}
+        isInitialTutorial={this.props.needsTutorial}
+        tutorial={this.props.tutorial}
+        finishTutorial={() => this.finishTutorial()}
+      />
+
+    const classification =
+      <View>
+        <Question
+          question={this.props.task.question}
+          workflowID={this.props.workflow.id}
+          taskHelp={this.props.task.help}
+        />
+        <View style={{ height: subjectDisplayHeight + 300, width: Dimensions.get('window').width}}>
+          <Swiper
+            ref={swiper => this.swiper = swiper}
+            cardHorizontalMargin={15}
+            keyExtractor={cardData => cardData.id}
+            cards={this.props.subjectLists}
+            renderCard={this.renderCard}
+            cardVerticalMargin={15}
+            marginTop={15}
+            backgroundColor="transparent"
+            onSwiped={this.onSwiped}
+            onSwipedRight={(cardIndex) => this.onAnswered(0, this.props.subjectLists[cardIndex])}
+            onSwipedLeft={(cardIndex) => this.onAnswered(1, this.props.subjectLists[cardIndex])}
+            onTapCard={this.onTapCard}
+            cardIndex={this.state.swiperIndex}
+            disableTopSwipe
+            disableBottomSwipe
+            outputRotationRange={['-30deg', '0deg', '30deg']}
+            nextCardYOffset={-15}
+            panXListener={(panX) => this.setState({ panX: panX}) }
+            swipeAnimationDuration={500}
+          />
+        </View>
+      </View>
+
+    const unlinkedTask = this.props.task.unlinkedTask ?
+      <View style={styles.unlinkedTaskContainer}>
+        <UnlinkedTask
+          unlinkedTaskKey={ this.props.task.unlinkedTask }
+          unlinkedTask={ this.props.workflow.tasks[this.props.task.unlinkedTask] }
+          annotation={ this.props.annotations[this.props.task.unlinkedTask] }
+          onAnswered={ this.onUnlinkedTaskAnswered }
+        />
+      </View>
+      : null
+
+    const needHelpText = this.props.task.help ?
+      <TouchableOpacity onPress={() => {this.setState({isModalVisible: true})}}>
+        <FontedText style={styles.needHelpText}> 
+          NEED SOME HELP WITH THIS TASK?
+        </FontedText>
+      </TouchableOpacity>
+      : null
+
+    const swipeTabs =
+      <SwipeTabs
+        guide={this.props.guide}
+        onLeftButtonPressed={ () => { this.swiper.swipeLeft() } }
+        onRightButtonPressed={ () => { this.swiper.swipeRight() }}
+        answers={this.props.answers}
+      />
+
+    const classificationPanel =
+      <View style={styles.classificationPanel}>
+        <ClassificationPanel
+          isFetching={ this.props.isFetching }
+          hasTutorial = { !R.isEmpty(this.props.tutorial) }
+          isQuestionVisible = {this.state.isQuestionVisible }
+          setQuestionVisibility = { this.setQuestionVisibility }
+        >
+          { this.state.isQuestionVisible ? classification : tutorial }
+          { this.state.isQuestionVisible ? unlinkedTask :null }
+        </ClassificationPanel>
+        { this.state.isQuestionVisible ? needHelpText : null }
+        { this.state.isQuestionVisible ? swipeTabs : null }
+        <FullScreenImage
+          source={{uri: this.state.fullScreenImageSource}}
+          isVisible={this.state.showFullSize}
+          handlePress={() => this.setState({ showFullSize: false })}
+        />
+        <TaskHelpModal
+          text={this.props.task.help}
+          isVisible={this.state.isModalVisible}
+          onCloseRequested={ () => this.setState({isModalVisible: false}) }
+        />
+      </View>
+
     return (
-      this.props.isFetching || !this.props.isSuccess ? <OverlaySpinner overrideVisibility={this.props.isFetching} /> : renderClassifierOrTutorial()
+      this.props.needsTutorial ? tutorial : classificationPanel
     )
   }
 }
 
 const styles = EStyleSheet.create({
+  classificationPanel: { 
+    flex: 1,
+    overflow: 'visible'
+  },
   subjectContainer: {
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 20,
   },
+  unlinkedTaskContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  needHelpText: {
+    textAlign: 'center',
+    marginTop: 15,
+    color: 'rgba(0,93,105,1)'
+  }
 })
 
 SwipeClassifier.propTypes = {
@@ -234,7 +304,11 @@ SwipeClassifier.propTypes = {
   needsTutorial: PropTypes.bool,
   guide: PropTypes.object,
   classifierActions: PropTypes.any,
-  navBarActions: PropTypes.any
+  navBarActions: PropTypes.any,
+  subjectLists: PropTypes.array,
+  subjectsSeenThisSession: PropTypes.array,
+  answers: PropTypes.array,
+  task: PropTypes.object
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SwipeClassifier)
