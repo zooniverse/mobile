0d180ea444b8cba3dc42639eddb40dbaaf7fbbe8
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-06-06 14:31:47 -0500

    Add iOS Native ImageSizer code
    This class calculates the size of images stored on file

---
+++ ios/ZooniverseMobile/ImageSizer.m
@@ -0,0 +1,32 @@
+//
+//  ImageSizer.m
+//  ZooniverseMobile
+//
+//  Created by Noah Malmed on 5/31/18.
+//  Copyright © 2018 Zooniverse. All rights reserved.
+//
+
+#import "ImageSizer.h"
+#import "RCTLog.h"
+
+@implementation ImageSizer
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(getImageDimensions:(NSString *)imagePath
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  UIImage * image = [UIImage imageWithContentsOfFile:imagePath];
+  if (!image) {
+    reject(@"ImageSizer Error", [@"Could not find image at path: " stringByAppendingString: imagePath], nil);
+  }
+  
+  CGSize imageSize = [image size];
+  NSNumber * width = [NSNumber numberWithDouble:imageSize.width];
+  NSNumber * height = [NSNumber numberWithDouble:imageSize.height];
+  
+  resolve(@{@"width": width, @"height": height});
+}
+
+@end
