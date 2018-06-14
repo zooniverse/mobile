a7bb04e237c3b6aabdbe1c752c30e01f66fcb4a5
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-06-14 14:08:06 -0500

    Refactor Projects to allow for refreshing
    This refactor involves:
     - Making one call for all of the workflows instead of one for each project
    - Making sure we don’t add the same data twice when we refresh data

--- src/actions/projects.js
+++ src/actions/projects.js
@@ -1,4 +1,3 @@
-import { Alert } from 'react-native';
 import apiClient from 'panoptes-client/lib/api-client'
 import { getAuthUser } from './auth'
 import * as R from 'ramda'
@@ -6,81 +5,107 @@ import * as R from 'ramda'
 import * as ActionConstants from '../constants/actions'
 import { isValidSwipeWorkflow } from '../utils/workflow-utils'
 
+const productionParams = {
+    mobile_friendly: true,
+    launch_approved: true,
+    live: true,
+    include: 'avatar',
+    sort: 'display_name',
+}
+
+const ownerParams = {
+    mobile_friendly: true,
+    live: false,
+    include: 'avatar',
+    sort: 'display_name',
+    current_user_roles: 'owner'
+}
+
+const collaboratorParams = {
+    mobile_friendly: true,
+    live: false,
+    include: 'avatar',
+    sort: 'display_name',
+    current_user_roles: 'collaborator'
+}
+
 export function fetchProjects() {
-    return (dispatch) => {
-        const productionParams = {
-            mobile_friendly: true,
-            launch_approved: true,
-            live: true,
-            include: 'avatar',
-            sort: 'display_name',
-        }
-
-        const ownerParams = {
-            mobile_friendly: true,
-            live: false,
-            include: 'avatar',
-            sort: 'display_name',
-            current_user_roles: 'owner'
-        }
-
-        const collaboratorParams = {
-            mobile_friendly: true,
-            live: false,
-            include: 'avatar',
-            sort: 'display_name',
-            current_user_roles: 'collaborator'
-        }
-
-        dispatch(addProjectsRequest)
-        getAuthUser().then( () => {
-            let projectCalls = []
-            let previewProjects = []
-            // Fetch production Projects
-            projectCalls.push(apiClient.type('projects').get(productionParams).then(loadWorkflows(dispatch, false)));
-
-            // Fetch Test Projects
-            projectCalls.push(apiClient.type('projects').get(ownerParams).then((projects) => {
-                projects.forEach((project) => dispatch(addOwnerProjectId(project)))
-                previewProjects = previewProjects.concat(projects);
-            }));
-            projectCalls.push(apiClient.type('projects').get(collaboratorParams).then((projects) => {
-                projects.forEach((project) => dispatch(addCollaboratorProjectId(project)))
-                previewProjects = previewProjects.concat(projects);
-            }));
-
-            Promise.all(projectCalls).then(() => {
-                loadWorkflows(dispatch, true, previewProjects).then(() => {
-                    dispatch(addProjectsSuccess);
-                })
+    return (dispatch) => { 
+        return new Promise((resolve, reject) => {
+            dispatch(addProjectsRequest)
+            getAuthUser().then( () => {
+                let projectCalls = []
+                let allProjects = []
                 
-            })
-            .catch((error) => { 
-                dispatch(addProjectsFailure);
-                Alert.alert( 'Error', 'The following error occurred.  Please close down Zooniverse and try again.  If it persists please notify us.  \n\n' + error,);
+                // Fetch production Projects
+                projectCalls.push(apiClient.type('projects').get(productionParams).then( projects => {
+                    const taggedProjects = tagProjects(projects, false)
+                    allProjects = allProjects.concat(taggedProjects)
+                }))
+
+                // Fetch Test Projects
+                projectCalls.push(apiClient.type('projects').get(ownerParams).then( projects => {
+                    const taggedProjects = tagProjects(projects, true)
+                    allProjects = allProjects.concat(taggedProjects)
+                    taggedProjects.forEach((project) => dispatch(addOwnerProjectId(project)))
+                }));
+                projectCalls.push(apiClient.type('projects').get(collaboratorParams).then( projects => {
+                    const taggedProjects = tagProjects(projects, true)
+                    allProjects = allProjects.concat(taggedProjects)
+                    taggedProjects.forEach((project) => dispatch(addCollaboratorProjectId(project)))
+                }));
+
+                // First Load the projects
+                Promise.all(projectCalls).then(() => {
+                    let projectDetailCalls = []
+                    projectDetailCalls.push(getWorkflowsForProjects(allProjects))
+                    const a = getAvatarsForProjects(allProjects)
+                    projectDetailCalls = projectDetailCalls.concat(a)
+                    // Then load the avatars and workflows
+                    Promise.all(projectDetailCalls)
+                    .then(() => {
+                        dispatch(addProjects(allProjects))
+                        dispatch(addProjectsSuccess);
+                        resolve()
+                    } )
+                    .catch((error) => { 
+                        dispatch(addProjectsFailure);
+                        reject(error)
+                    })
+                })
+                .catch((error) => { 
+                    dispatch(addProjectsFailure);
+                    reject(error)
+                })
             })
         })
     }
 }
 
-const loadWorkflows = R.curry((dispatch, arePreview, projects) => {            
-    const promises = []
-    projects.forEach((project) => {
-        promises.push(apiClient.type('avatars').get(project.links.avatar.id).then((avatar) => {
+const getAvatarsForProjects = projects => {
+    return projects.map( project => {
+        return apiClient.type('avatars').get(project.links.avatar.id)
+        .then((avatar) => {
             project.avatar_src = avatar.src
         })
         // Stub out avatar rejection because it is optional for projects to have avatars
-        .catch(() => {}));
+        .catch(() => {});
+    })
+}
 
-        promises.push(project.get('workflows', {mobile_friendly: true, active: true}).then((workflows) => {
-            project.workflows = tagSwipeFriendly(workflows)
+const getWorkflowsForProjects = projects => {            
+    const projectIds = projects.map((project) => project.id)
+    return apiClient.type('workflows').get({mobile_friendly: true, active: true, project_id: projectIds})
+    .then(workflows => {
+        workflows.forEach( workflow => {
+            workflow.swipe_verified = workflow.mobile_friendly && isValidSwipeWorkflow(workflow)
+            const project = projects.find( project => project.id === workflow.links.project )
+            if (!project.workflows.find((projectWorkflow) => projectWorkflow.id === workflow.id)) {
+                project.workflows = R.append(workflow, project.workflows)
+            }
         })
-        .catch(() => project.workflows = []));
     })
-    return Promise.all(promises).then(() => {
-        return dispatch(addProjects(projects, arePreview))
-    })
-});
+};
 
 const addOwnerProjectId = (project) => ({
     type: ActionConstants.ADD_OWNER_PROJECT_ID,
@@ -104,15 +129,16 @@ const addProjectsFailure = {
     type: ActionConstants.PROJECTS_FAILURE
 }
 
-const addProjects = (projects, arePreview) => ({
+const addProjects = projects => ({
     type: ActionConstants.ADD_PROJECTS, 
-    arePreview,
     projects
 })
 
-function tagSwipeFriendly(workflows) {
-    return workflows.map((workflow) => {
-        workflow.swipe_verified = workflow.mobile_friendly && isValidSwipeWorkflow(workflow)
-        return workflow
-    });
+const tagProjects = (projects, arePreview) => {
+    const taggedProjects = projects.map( project => {
+        project.isPreview = arePreview
+        if (!project.workflows) project.workflows = []
+        return project
+    })
+    return taggedProjects
 }
