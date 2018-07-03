30e943fdb769a30e66c2ba6216106f39d8505276
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-07-03 13:27:28 -0500

    Add New Setting Page and component

---
+++ src/components/settings/Settings.js
@@ -0,0 +1,175 @@
+import React from 'react'
+import {
+  FlatList,
+  ScrollView,
+  View
+} from 'react-native'
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import { SettingsToggle, SettingHeader } from './settingsComponents'
+import NavBar from '../NavBar'
+import FontedText from '../common/FontedText'
+import Separator from '../common/Separator'
+import { connect } from 'react-redux'
+import {
+  checkPushPermissions,
+} from '../../actions/index'
+import GoogleAnalytics from 'react-native-google-analytics-bridge'
+import * as settingsActions from '../../actions/settings'
+
+GoogleAnalytics.trackEvent('view', 'Notification Settings')
+
+const mapStateToProps = (state) => {
+  const {
+    showAllWorkflows,
+    enableNotifications,
+    newProjectNotifications,
+    newBetaNotifications,
+    urgentHelpNotification,
+    projectSpecificNotifications
+  } = state.settings
+
+  return {
+    notifications: state.main.notifications,
+    projectList: state.projects.projectList || [],
+    isFetching: state.main.isFetching,
+    errorMessage: state.main.errorMessage,
+    pushEnabled: state.main.pushEnabled,
+    showAllWorkflows,
+    enableNotifications,
+    newProjectNotifications,
+    newBetaNotifications,
+    urgentHelpNotification,
+    projectSpecificNotifications
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  checkPushPermissions(){
+    dispatch(checkPushPermissions())
+  },
+  settingsActions: bindActionCreators(settingsActions, dispatch)
+})
+
+export class Settings extends React.Component {
+  componentDidMount() {
+    this.props.checkPushPermissions()
+  }
+
+  static renderNavigationBar() {
+    return <NavBar title={'Settings'} showBack={true} />;
+  }
+
+  renderSettingsToggle = ({item}) => {
+    const {subscribed, id, display_name } = item
+    return (
+      <SettingsToggle
+        value={subscribed}
+        onToggle={() => {this.props.settingsActions.updateProjectSubsciption(id, !subscribed)}}
+        title={display_name}
+        disabled={!this.props.enableNotifications}
+      />
+    )
+  }
+
+  render() {
+    
+
+    return (
+      <ScrollView contentContainerStyle={{padding: 25}}>
+        <SettingHeader text="General Settings" />
+        <View style={{paddingTop: 20, paddingBottom: 35}}>
+          <SettingsToggle
+            title="Show all workflows"
+            description="Includes non-native workflows"
+            onToggle={() => {this.props.settingsActions.updateShowAllWorkflow(!this.props.showAllWorkflows)}}
+            value={this.props.showAllWorkflows}
+          />
+        </View>
+        <Separator style={{paddingBottom: 30}} color={'rgba(151, 151, 151, 1)'}/>
+        <View>
+          <SettingHeader text="Notification Settings" />
+        </View>
+        <FontedText style={styles.disclosureText}> 
+          The Zooniverse app can occassionally send you updates about new projects or projects you might be interested in.
+        </FontedText>
+        <SettingsToggle
+          style={styles.toggleSpacing}
+          onToggle={() => {this.props.settingsActions.updateEnableNotifications(!this.props.enableNotifications)}}
+          title='Enable Notifications'
+          value={this.props.enableNotifications}
+        />
+        <SettingsToggle
+          style={styles.toggleSpacing}
+          onToggle={() => {this.props.settingsActions.updateNewProjectNotifications(!this.props.newProjectNotifications)}}
+          title='New Projects'
+          value={this.props.newProjectNotifications}
+          disabled={!this.props.enableNotifications}
+        />
+        <SettingsToggle
+          style={styles.toggleSpacing}
+          onToggle={() => {this.props.settingsActions.updateBetaNotifications(!this.props.newBetaNotifications)}}
+          title='New Beta Projects'
+          value={this.props.newBetaNotifications}
+          disabled={!this.props.enableNotifications}
+        />
+        <SettingsToggle
+          style={styles.toggleSpacing}
+          onToggle={() => {this.props.settingsActions.updateUrgentHelpNotifications(!this.props.urgentHelpNotification)}}
+          title='Urgent Help Alerts'
+          description='Notifications when projects need timely help'
+          value={this.props.urgentHelpNotification}
+          disabled={!this.props.enableNotifications}
+        />
+        <FontedText style={styles.projectHeader}>
+          Project-specific Notifications
+        </FontedText>
+        <FlatList
+          data={this.props.projectSpecificNotifications}
+          renderItem={this.renderSettingsToggle}
+          ItemSeparatorComponent={() => <View style={{paddingTop: 10}}/>}
+          keyExtractor={ item => item.id }
+        />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = EStyleSheet.create({
+  toggleSpacing: {
+    paddingBottom: 15
+  },
+  disclosureText: {
+    paddingBottom: 20
+  },
+  projectHeader: {
+    color: '$headerGrey',
+    fontSize: 18,
+    paddingBottom: 15
+  }
+});
+
+Settings.propTypes = {
+  notifications: PropTypes.shape({
+    general: PropTypes.bool,
+  }),
+  settings: PropTypes.shape({
+    promptForWorkflow: PropTypes.bool,
+  }),
+  projectList: PropTypes.array,
+  isFetching: PropTypes.bool,
+  pushEnabled: PropTypes.bool,
+  errorMessage: PropTypes.string,
+  updateGeneralNotification: PropTypes.func,
+  checkPushPermissions: PropTypes.func,
+  showAllWorkflows: PropTypes.bool,
+  enableNotifications: PropTypes.bool,
+  newProjectNotifications: PropTypes.bool,
+  newBetaNotifications: PropTypes.bool,
+  urgentHelpNotification: PropTypes.bool,
+  projectSpecificNotifications: PropTypes.array,
+  settingsActions: PropTypes.any
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings)
---
+++ src/components/settings/settingsComponents.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import {
+  Switch,
+  View
+} from 'react-native'
+import PropTypes from 'prop-types';
+import FontedText from '../common/FontedText'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import theme from '../../theme'
+
+export const SettingHeader = ({text}) => {
+    return (
+        <FontedText style={styles.settingHeader}>
+            {text}
+        </FontedText>
+    )
+}
+  
+export const SettingsToggle = ({onToggle, title, description, value, disabled, style}) => {
+    return (
+        <View style={style}>
+            <View style={styles.settingsToggleContainer}>
+                <Switch
+                    disabled={disabled}
+                    onTintColor={theme.$zooniverseTeal}
+                    onValueChange={onToggle}
+                    value={value}
+                />
+                <View style={styles.toggleTextContainer}>
+                    <FontedText style={styles.toggleTitle}>
+                        {title}
+                    </FontedText>
+                    {description ? 
+                        <FontedText>
+                            {description}
+                        </FontedText>
+                    : null }
+                </View>
+            </View>
+        </View>
+    )
+}
+
+SettingHeader.propTypes = {
+    text: PropTypes.string
+}
+
+SettingsToggle.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    onToggle: PropTypes.func,
+    style: PropTypes.any,
+    value: PropTypes.bool,
+    disabled: PropTypes.bool
+}
+  
+const styles = EStyleSheet.create({
+    settingsToggleContainer: {
+        flexDirection: 'row'
+    },
+    toggleTextContainer: {
+        flex: 1,
+        paddingLeft: 15,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    toggleTitle: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: '$headerGrey'
+    },
+    settingHeader: {
+        color: '$headerGrey',
+        fontSize: 26
+    }
+});
