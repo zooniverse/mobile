8b9c1ede88af67a7b8b9916b18acf3b5a5837759
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-07-17 11:30:31 -0500

    Add BetaFeedbackView

---
+++ src/components/classifier/BetaFeedbackView.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {
+    TouchableOpacity
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import PropTypes from 'prop-types'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import FontedText from '../common/FontedText'
+
+
+const BetaFeedbackView = (props) => {
+    return (
+        <TouchableOpacity 
+            style={styles.touchableContainer}
+            onLayout={props.onLayout}
+            onPress={props.onPress}
+        >
+            <FontedText style={styles.title}> Provide Beta Feedback For This Project </FontedText>
+            <Icon style={styles.icon} name="arrow-right" />
+        </TouchableOpacity>
+    )
+}
+
+const styles = EStyleSheet.create({
+    touchableContainer: {
+        backgroundColor: '$zooniverseTeal',
+        paddingVertical: 10,
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+    title: {
+        paddingRight: 10,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    icon: {
+        alignSelf: 'center',
+        color: 'white'
+    }
+})
+
+BetaFeedbackView.propTypes = {
+    onPress: PropTypes.func,
+    onLayout: PropTypes.func,
+}
+
+export default BetaFeedbackView
+
+
+
