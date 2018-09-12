96f529f49a47c7d89815aae57c58aafd2cd188f1
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-09-12 16:02:58 -0500

    Update for hound

--- fastlane/VersionUtils.rb
+++ fastlane/VersionUtils.rb
@@ -1,35 +1,31 @@
 module VersionUtils
 
-  def VersionUtils.versionStringIsValid (versionString)
-    versionStringArray = versionString.split(".")
-  
-    for numberString in versionStringArray do
-      convertedString = Integer(numberString) rescue false
-      if !convertedString then
-        return false
-      end
+  def VersionUtils.versionStringIsValid(version_string)
+    version_string_array = version_string.split(".")
+    for number_string in version_string_array do
+      is_number = /\A\d+\z/ =~ number_string
+      unless is_number; false end
     end
 
-    return true
+    true
   end
 
-  def VersionUtils.versionStringIsCorrectLength (versionString)
-    versionStringArray = versionString.split(".")
-    return versionStringArray.length == 3
+  def VersionUtils.versionStringIsCorrectLength(version_string)
+    version_string.split(".").length == 3
   end
-  
-  def VersionUtils.isNewVersionBiggerThanOldVersion (newVersionNumberString, oldVersionNumberString)
-    newVersionArray = newVersionNumberString.split(".").map { |numberString| numberString.to_i }
-    oldVersionArray = oldVersionNumberString.split(".").map { |numberString| numberString.to_i }
-  
+
+  def VersionUtils.isNewVersionBiggerThanOldVersion(new_version_number_string, old_version_number_string)
+    new_version_array = new_version_number_string.split(".").map(&:to_i)
+    old_version_array = old_version_number_string.split(".").map(&:to_i)
     for index in 0..2
-      if newVersionArray[index] > oldVersionArray[index] then
+      if new_version_array[index] > old_version_array[index]
         return true
       end
-      if newVersionArray[index] < oldVersionArray[index] then
+      if new_version_array[index] < old_version_array[index]
         return false
       end
     end
-    return false
+    false
   end
+
 end
