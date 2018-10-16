ffdd0f4d38a91e7f41072d9eae9ca83903ee4ffa
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-10-16 11:35:03 -0500

    Add New editing and deleting logic
    It turns out that Svgs have trouble with touch events in modals in react-native.
    
    So I had to rewrite how we tackle this problem. Basically, instead of adding pan responders to the svgs, the svgs report their location to ShapeEditorSvg and that component has a pan handling overlay that uses the shape’s locations to understand what the user wants to do when they touch and drag

--- src/components/Markings/components/CloseButtonSVG.js
+++ src/components/Markings/components/CloseButtonSVG.js
@@ -22,7 +22,7 @@ class CloseButtonSVG extends Component {
             >
                 <Symbol id="symbol" viewBox="0 0 512 512" >
                     <Path
-                        fill="black"
+                        fill={this.props.color}
                         d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm101.8-262.2L295.6 256l62.2 62.2c4.7 4.7 4.7 12.3 0 17l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L256 295.6l-62.2 62.2c-4.7 4.7-12.3 4.7-17 0l-22.6-22.6c-4.7-4.7-4.7-12.3 0-17l62.2-62.2-62.2-62.2c-4.7-4.7-4.7-12.3 0-17l22.6-22.6c4.7-4.7 12.3-4.7 17 0l62.2 62.2 62.2-62.2c4.7-4.7 12.3-4.7 17 0l22.6 22.6c4.7 4.7 4.7 12.3 0 17z"
                     />
                 </Symbol>
@@ -31,16 +31,15 @@ class CloseButtonSVG extends Component {
                     href="#symbol"
                     width="20" height="20"
                 />
-                <Rect width="20" height="20" onPressIn={this.props.onPress} fill="transparent" />
+                <Rect onLayout={this.props.onLayout} width="20" height="20" fill="transparent" />
             </G>
         )
     }
 }
 
 CloseButtonSVG.propTypes = {
-    x: PropTypes.number,
-    y: PropTypes.number,
-    onPress: PropTypes.func
+    color: PropTypes.string,
+    onLayout: PropTypes.func,
 }
 
 export default CloseButtonSVG
