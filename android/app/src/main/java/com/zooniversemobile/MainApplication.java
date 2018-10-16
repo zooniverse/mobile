e94f97b38d08b2157c1686bf05f68fd2df2409c7
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-10-16 11:14:29 -0500

    Update dependencies
    Update react-native-vetor-icons to include Font Awesome 5 icons.

--- android/app/src/main/java/com/zooniversemobile/MainApplication.java
+++ android/app/src/main/java/com/zooniversemobile/MainApplication.java
@@ -5,13 +5,14 @@ import android.app.Application;
 
 import com.evollu.react.fcm.FIRMessagingPackage;
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.horcrux.svg.SvgPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.facebook.soloader.SoLoader;
 
@@ -30,6 +31,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new BlurViewPackage(),
             new RNFetchBlobPackage(),
             new SplashScreenReactPackage(),
