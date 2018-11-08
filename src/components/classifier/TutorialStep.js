8f7cc4f0a063529228873a9eb23cfcbd5524eaa3
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-11-08 14:21:06 -0600

    Add TutorialStep
    This component encapsulates a single tutorial step

---
+++ src/components/classifier/TutorialStep.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import {
+    Dimensions,
+    ScrollView,
+    View
+} from 'react-native'
+import PropTypes from 'prop-types'
+import Markdown from 'react-native-simple-markdown'
+
+import FittedImage from '../common/FittedImage' 
+
+const ImageWidth = Dimensions.get('window').width - 100
+class TutorialStep extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            width: 1,
+            displayStep: this.props.mediaUri === null
+        }
+
+        this.onLayout = this.onLayout.bind(this)
+    }
+
+    onLayout({nativeEvent}) {
+        this.setState({
+            width: nativeEvent.layout.width
+        })
+    }
+
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                <View style={styles.container}>
+                    <View style={styles.contentContainer} onLayout={this.onLayout}>
+                        {
+                            this.props.mediaUri ?
+                                <FittedImage 
+                                    maxWidth={ImageWidth}
+                                    maxHeight={ImageWidth}
+                                    source={{ uri: this.props.mediaUri }}
+                                    onLoad={() => this.setState({displayStep: true})}
+                                />
+                            : null
+                        }
+
+                        <View style={styles.markdown} >
+                            <Markdown >
+                                { this.props.markdownContent }
+                            </Markdown>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        )
+    }
+}
+
+const styles = {
+    markdown: {
+        flex: 1,
+        marginTop: 15
+      },
+      container: {
+          flex: 1
+      },
+      contentContainer: {
+          flex: 1,
+          margin: 25
+      }
+}
+
+TutorialStep.propTypes = {
+    markdownContent: PropTypes.string,
+    mediaUri: PropTypes.string,
+    width: PropTypes.number
+}
+
+export default TutorialStep        
