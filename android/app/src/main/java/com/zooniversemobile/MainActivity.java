ecd44f710a77ba514d79f51c90e00772c438d091
Will Granger <twgranger@gmail.com>
2019-11-22 14:16:39 -0600

    Allow App to Build on Android

--- android/app/src/main/java/com/zooniversemobile/MainActivity.java
+++ android/app/src/main/java/com/zooniversemobile/MainActivity.java
@@ -10,7 +10,7 @@ import android.content.res.Configuration;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.facebook.react.ReactActivity;
--- android/app/src/main/java/com/zooniversemobile/MainApplication.java
+++ android/app/src/main/java/com/zooniversemobile/MainApplication.java
@@ -1,24 +1,20 @@
 package com.zooniversemobile;
 
-import android.support.multidex.MultiDexApplication;
+import androidx.multidex.MultiDexApplication;
 import android.app.Application;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.facebook.soloader.SoLoader;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.facebook.react.BuildConfig;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
@@ -31,23 +27,12 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
 
     @Override
     protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      packages.add(new RNFirebasePackage())
-      packages.add(new RNFirebaseMessagingPackage())
-      return packages
-
-      // return Arrays.<ReactPackage>asList(
-      //     new MainReactPackage(),
-      //       new VectorIconsPackage(),
-      //       new SvgPackage(),
-      //       new BlurViewPackage(),
-      //       new RNFetchBlobPackage(),
-      //       new SplashScreenReactPackage(),
-      //       new MainApplicationPackage(),
-      //       new GoogleAnalyticsBridgePackage(),
-      //       new RNDeviceInfo()
-      // );
+      packages.add(new RNFirebasePackage());
+      packages.add(new RNFirebaseMessagingPackage());
+      return packages;
     }
 
     @Override
