310475071fe8e6615004ae3adb63a209fb9057d4
Chelsea Troy <chelsea.dommert@gmail.com>
2020-07-14 15:59:01 -0500

    Make several changes necessary to get the app with this version of RN running on both an Android emulator and an iOS simulator:
    + Install React Native Vector Icons in project
    + Remove a patch we implemented for the old version of React Native Vector Icons (which didn't quite work on our app otherwise)
    + Use RN Alert instead of AlertIOS
    + Remove custom color for firebase notifications on Android (the latest version of Firebase asks clients to define their custom color in a different way than old versions, and it appears to be backward-breaking. Furthermore, when using the new method, I run into errors that say the color is not available. I tried this with our custom colors, with standard colors, and even with hex values. I timeboxed the effort to an hour, and at an hour I removed custom coloring for firebase notifications on Android. This got the app to run.)

--- android/app/src/main/java/com/zooniversemobile/MainApplication.java
+++ android/app/src/main/java/com/zooniversemobile/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.horcrux.svg.SvgPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
