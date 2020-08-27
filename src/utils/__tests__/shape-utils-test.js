416f243df13a3ddd46507ceca425cc6c147b2fef
Chelsea Troy <chelsea.dommert@gmail.com>
2020-08-27 14:46:30 -0500

    Fix tests to work within our current schema (formerly they were failing)

--- src/utils/__tests__/shape-utils-test.js
+++ src/utils/__tests__/shape-utils-test.js
@@ -1,59 +1,57 @@
-import { calculateDeleteButtonPosition } from '../shapeUtils'
+import {calculateDeleteButtonPosition} from '../shapeUtils'
 
-describe('calculateDeleteButtonPosition', () => {
-  describe('when the rects are in the corners', () => {
-    const topRight = calculateDeleteButtonPosition(
-      80, 0, 20, 20, 100, 1
+it('moves delete button when the rects are in the corners', () => {
+    const topRightCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        80, 0, 20, 20, 100, 1
     )
-    expect(topRight).toEqual({ x: 54, y: 0 })
+    expect(topRightCornerDeleteButtonPosition).toEqual({x: 54, y: 0})
 
-    const bottomRight = calculateDeleteButtonPosition(
-      80, 80, 20, 20, 100, 1
+    const bottomRightCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        80, 80, 20, 20, 100, 1
     )
-    expect(bottomRight).toEqual({ x: 54, y: 67 })
+    expect(bottomRightCornerDeleteButtonPosition).toEqual({x: 54, y: 67})
 
-    const bottomLeft = calculateDeleteButtonPosition(
-      0, 80, 20, 20, 100, 1
+    const bottomLeftCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        0, 80, 20, 20, 100, 1
     )
-    expect(bottomLeft).toEqual({ x: 20, y: 67 })
+    expect(bottomLeftCornerDeleteButtonPosition).toEqual({x: 20, y: 67})
 
-    const topLeft = calculateDeleteButtonPosition(
-      0, 0, 20, 20, 100, 1
+    const topLeftCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        0, 0, 20, 20, 100, 1
     )
-    expect(topLeft).toEqual({ x: 20, y: 0 })
-  })
+    expect(topLeftCornerDeleteButtonPosition).toEqual({x: 20, y: 0})
+})
 
-  describe('when the rects are flipped and in the corners', () => {
-    const topRight = calculateDeleteButtonPosition(
-      100, 20, -20, -20, 100, 1
+it('moves delete button when the rects are flipped and in the corners', () => {
+    const topRightCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        100, 20, -20, -20, 100, 1
     )
-    expect(topRight).toEqual({ x: 54, y: 0 })
+    expect(topRightCornerDeleteButtonPosition).toEqual({x: 54, y: 0})
 
-    const bottomRight = calculateDeleteButtonPosition(
-      100, 100, -20, -20, 100, 1
+    const bottomRightCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        100, 100, -20, -20, 100, 1
     )
-    expect(bottomRight).toEqual({ x: 54, y: 67 })
+    expect(bottomRightCornerDeleteButtonPosition).toEqual({x: 54, y: 67})
 
-    const bottomLeft = calculateDeleteButtonPosition(
-      20, 100, -20, -20, 100, 1
+    const bottomLeftCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        20, 100, -20, -20, 100, 1
     )
-    expect(bottomLeft).toEqual({ x: 20, y: 67 })
+    expect(bottomLeftCornerDeleteButtonPosition).toEqual({x: 20, y: 67})
 
-    const topLeft = calculateDeleteButtonPosition(
-      20, 20, -20, -20, 100, 1
+    const topLeftCornerDeleteButtonPosition = calculateDeleteButtonPosition(
+        20, 20, -20, -20, 100, 1
     )
-    expect(topLeft).toEqual({ x: 20, y: 0 })
-  })
+    expect(topLeftCornerDeleteButtonPosition).toEqual({x: 20, y: 0})
+})
 
-  describe('when the rects fill the screen', () => {
-    const fullScreen = calculateDeleteButtonPosition(
-      0, 0, 100, 100, 100, 1
+it('moves delete button when the rects fill the screen', () => {
+    const fullScreenDeleteButtonPosition = calculateDeleteButtonPosition(
+        0, 0, 100, 100, 100, 1
     )
-    expect(fullScreen).toEqual({ x: 0, y: 0 })
+    expect(fullScreenDeleteButtonPosition).toEqual({x: 0, y: 0})
 
-    const fullScreenFlipped = calculateDeleteButtonPosition(
-      100, 100, -100, -100, 100, 1
+    const fullScreenFlippedDeleteButtonPosition = calculateDeleteButtonPosition(
+        100, 100, -100, -100, 100, 1
     )
-    expect(fullScreenFlipped).toEqual({ x: 0, y: 0 })
-  })
+    expect(fullScreenFlippedDeleteButtonPosition).toEqual({x: 0, y: 0})
 })
