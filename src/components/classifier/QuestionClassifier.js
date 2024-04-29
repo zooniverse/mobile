1aa048ef3ccc2e70c3d68c49d34cc11a98bdebe7
Cory Chambers <cory@zooniverse.org>
2024-04-29 09:12:11 -0400

    Fixed spacing issue in import.

--- src/components/classifier/QuestionClassifier.js
+++ src/components/classifier/QuestionClassifier.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import {    ScrollView,
-    View
-} from 'react-native';
+import { ScrollView, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
