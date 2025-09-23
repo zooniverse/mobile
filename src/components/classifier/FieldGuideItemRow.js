5698bc793c95328f741e219a52dc303a8f575116
Cory Chambers <cory@zooniverse.org>
2025-09-23 17:36:41 -0400

    Check that props has icons before calling a key on an object that might be null.

--- src/components/classifier/FieldGuideItemRow.js
+++ src/components/classifier/FieldGuideItemRow.js
@@ -9,8 +9,8 @@ import { useTranslation } from 'react-i18next';
 import { getCurrentProjectLanguage } from '../../i18n';
 
 const FieldGuideItemRow = (props) => {
-    const { t } = useTranslation();
-    const itemIcon = props.icons[props.item.icon]
+    const { t } = useTranslation();    
+    const itemIcon = props?.icons ? props.icons[props?.item?.icon] : undefined
     return (
       <TouchableOpacity onPress={props.onPress} style={styles.itemRow}>
         {itemIcon !== undefined && itemIcon.src ? (
