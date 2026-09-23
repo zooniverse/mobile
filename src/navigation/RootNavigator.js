4059de1732ba5aa62c00938baec7212f93a32d20
Cory Chambers <cory@zooniverse.org>
2026-09-23 10:23:39 -0400

    Turn off navigation animation for android.

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Platform } from 'react-native';
 
 import {
   NavigationContainer,
@@ -54,7 +55,9 @@ const StackNavigator = () => {
       initialRouteName={PageKeys.ZooniverseApp}
       screenOptions={{
         header: ({ navigation }) => <NavBar navigation={navigation} />,
-        gestureEnabled: false
+        gestureEnabled: false,
+        // Diagnostic: test whether Android transitions cause persistent transparency.
+        animation: Platform.OS === 'android' ? 'none' : 'default',
       }}>
       <Stack.Screen
         name={PageKeys.SignIn}
